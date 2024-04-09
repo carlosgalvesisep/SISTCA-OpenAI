6e1a836fe6ffb94238b11cab8de2f26b5401270e
carlos <cgalves@invisiblecloud.pt>
2024-04-09 18:09:02 +0100

    Added translation exercise

---
+++ Tutorials/Whisper/translate.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+audio_file= open("Tutorials/Whisper/audio.wav", "rb")
+translation = client.audio.translations.create(
+    model = "whisper-1", 
+    file = audio_file
+)
+print(translation.text)
