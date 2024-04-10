ee46cbee1146704eea5c8c485fb1e99922ee2afb
jpedroal11 <1211066@isep.ipp.pt>
2024-04-10 16:48:10 +0100

    chat completion tutorial created and updated

---
+++ Tutorials/chatCompletion/main.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
