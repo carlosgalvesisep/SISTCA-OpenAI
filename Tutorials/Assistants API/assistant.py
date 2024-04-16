d8f6f6a34207b54f9f09e1e13504d520f8d90f6b
carlos <cgalves@invisiblecloud.pt>
2024-04-16 22:42:59 +0100

    initial implementation of assistants api

---
+++ Tutorials/Assistants API/assistant.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+  
+assistant = client.beta.assistants.create(
+  name="Math Tutor",
+  instructions="You are a personal math tutor. Write and run code to answer math questions.",
+  # tools=[{"type": "code_interpreter"}],
+  model="gpt-3.5-turbo",
+)
+
+thread = client.beta.threads.create()
+
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+)
+
+print(message.content[0].text.value)
+
+run = client.beta.threads.runs.create_and_poll(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  instructions="Please address the user as Jane Doe. The user has a premium account."
+)
+
+if run.status == 'completed': 
+  messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+  )
+
+  print(messages.data[0].content[0].text.value)
+else:
+  print(run.status)
