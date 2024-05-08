8885434cffa5b7acbcca95e2ee90017d6601bb4d
sousap02 <104429285+sousap02@users.noreply.github.com>
2024-05-08 15:47:33 +0100

    Assistant

--- Tutorials/Assistants API/assistant.py
+++ Tutorials/Assistants API/assistant.py
@@ -1,19 +1,20 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from openai import OpenAI
 from runners.standard_run import std_run
 from runners.streaming_run import streaming_run
 
-load_dotenv()
+#load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
+#api_key = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI(api_key=api_key)
+client = OpenAI(#api_key=api_key
+                )
   
 assistant = client.beta.assistants.create(
   name="Math Tutor",
   instructions="You are a personal math tutor. Write and run code to answer math questions.",
-  # tools=[{"type": "code_interpreter"}],
+  #tools=[{"type": "code_interpreter"}],
   model="gpt-3.5-turbo",
 )
 
@@ -27,5 +28,5 @@ message = client.beta.threads.messages.create(
 
 print(message.content[0].text.value)
 
-#print(std_run(thread.id, assistant.id))
-print(streaming_run(thread.id, assistant.id))
+print(std_run(thread.id, assistant.id))
+#print(streaming_run(thread.id, assistant.id))
